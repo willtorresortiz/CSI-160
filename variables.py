36551e78b0dad46f6b9acc87dfea7ab7b9981bc2
Will <shxdow@stu-18-145-171-184.champlain.edu>
2026-08-31 14:14:00 -0400

    variables.py added, going over variables and printing variables

---
+++ variables.py
@@ -0,0 +1,44 @@
+x = 2 # single line comment, the data type is an integer
+y= 1
+name = "John Doe" #Data type is string
+m = 3.14 #Data type is floating point/float
+a = True #Data type is boolean
+b = False
+x = 15
+answer = "False" #String Value
+'''
+This is a block/multi-line comment
+It can be used over several lines in your
+program.
+
+
+The example below is a horrible way to work with variables
+'''
+last_name = 'Smith' #Still a string value
+lastName = 'Jones'
+LastName = "" #Empty String
+lastname = ""
+Whatisyourlastnameplease10101010101 = ""
+address1 = '15 Weaver Street'
+city = 'Burlington'
+state = 'VT'
+zip = '05401'
+
+title = "There's a Lot to Learn with Python"
+z = x + y 
+
+#An example of concatenation
+print("The sum of x + y equals " + str(z))
+
+print('The Value of x equals',x)
+print('The Value of y equals',y)
+print('The Value of z equals',z)
+
+print("The sum of",x,"plus",y,"equals",z)
+print(f"The sum of {x} + {y} equals {z}")
+
+age = input("what is your age? ") #Input treats user input as string
+age = int(age) #The int() method converts a string to an integer 
+year = 2026
+birth_year = 2026 - age
+print("you were born in", birth_year)
